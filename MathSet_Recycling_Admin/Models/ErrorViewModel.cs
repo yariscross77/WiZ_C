f396e733d32a0d0f0cb003d38e0630529fa1f01b
金谷　健人 <92280900+yariscross77@users.noreply.github.com>
2022-11-11 03:18:25 +0900

    Delete MathSet_Recycling_Admin directory

--- MathSet_Recycling_Admin/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MathSet_Recycling_Admin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
