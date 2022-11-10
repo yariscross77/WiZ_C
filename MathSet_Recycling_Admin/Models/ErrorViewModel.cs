a1a21c202ff4613310a73628b0852df40b1c0672
Kento.Kanaya <zeal20410028@fsgcl.jp>
2022-11-11 01:49:43 +0900

    管理者画面

---
+++ MathSet_Recycling_Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MathSet_Recycling_Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
